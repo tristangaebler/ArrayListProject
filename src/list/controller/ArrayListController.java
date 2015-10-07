a8c6999f62e5a65606425c0f5f6da08b13627ac1
tristangaebler <Tristan.Gaebler@gmail.com>
2015-10-07 09:54:31 -0600

    Added the showList Method

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -1,9 +1,29 @@
 package list.controller;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class ArrayListController
-{
+{	
+	private ArrayList<String> firstWords;
+
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
 	public void start()
 	{
-		
+		firstWords.add("lisauhgiuashgi");
+		firstWords.add("bababababababababababababab");
+	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) + " is at spot# " + spot );
+		}
 	}
 }
